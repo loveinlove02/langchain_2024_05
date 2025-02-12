d448a6549b96a687e76d05a27340fcaa0f09813b
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-02-12 16:58:52 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,30 +1,77 @@
-_type: "prompt"
-template: |
-  You are an expert Prompt Writer for Large Language Models.
-  Your goal is to improve the prompt given below for {task} :
-  --------------------
+import streamlit as st
 
-  Prompt: {question}
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import ChatMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_teddynote.prompts import load_prompt
 
-  --------------------
+from dotenv import load_dotenv
+import os
 
-  Here are several tips on writing great prompts:
-  -------
-  Start the prompt by stating that it is an expert in the subject.
-  Put instructions at the beginning of the prompt and use ### or to separate the instruction and context 
-  Be specific, descriptive and as detailed as possible about the desired context, outcome, length, format, style, etc 
-  ---------
-  Here's an example of a great prompt:
-  As a master YouTube content creator, develop an engaging script that revolves around the theme of "Exploring Ancient Ruins."
-  Your script should encompass exciting discoveries, historical insights, and a sense of adventure.
-  Include a mix of on-screen narration, engaging visuals, and possibly interactions with co-hosts or experts.
-  The script should ideally result in a video of around 10-15 minutes, providing viewers with a captivating journey through the secrets of the past.
+load_dotenv(verbose=True)
+key = os.getenv('OPENAI_API_KEY')
 
-  Example:
-  "Welcome back, fellow history enthusiasts, to our channel! Today, we embark on a thrilling expedition..."
-  -----
-  Now, improve the prompt.
-  Write in Korean. Answer must be wrapped in triple quotes.
+st.title('chatbot')
 
-  IMPROVED PROMPT:
-input_variables: ["task", "question"]
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
+with st.sidebar:
+    clear_btn = st.button('대화 다시')
+    task_input = st.text_input('TASK 입력', '')
+
+# 대화 내용 출력
+def print_messages():
+    for chat_message in st.session_state['messages']:
+        with st.chat_message(chat_message.role):
+            st.write(chat_message.contnet)
+
+# 대화 내용 추가
+def add_messages(role, messages):
+    st.session_state['messages'].append(
+        ChatMessage(role=role, content=messages)
+    )
+
+# chain
+def create_chain(task=''):
+    prompt = load_prompt('./prompts/prompt-maker.yaml')
+
+    if task:
+        prompt = prompt.partial(task=task)
+
+    llm = ChatOpenAI(
+        api_key=key,
+        model='gpt-4o-mini',
+        temperature=0
+    )
+
+    output_parer = StrOutputParser()
+    chain = prompt | llm| output_parer
+    return  chain
+
+if clear_btn:                           # 버튼을 클릭 하면
+    st.session_state['messages'] = []   # 대화 내용 삭제
+
+print_messages()
+
+user_input = st.chat_input('궁긍한 내용을 물어 보세요')
+
+if user_input:
+    with st.chat_message('user'):
+        st.write(user_input)
+
+    chain = create_chain(task=task_input)
+    response = chain.stream({'question': user_input})
+
+    with st.chat_message('assistant'):
+        container = st.empty()
+        answer = ''
+
+        for token in response:
+            answer = answer + token
+            container.markdown(answer)       # 화면에 따따따 출력
+
+
+    add_messages('user', user_input)
+    add_messages('assistant', answer)
