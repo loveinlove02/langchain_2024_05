60874ae785cddce0a7b553615b2c0bf361478e75
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-02-18 18:10:57 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,25 +1,40 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from langchain.agents import tool
 
-# url = "https://n.news.naver.com/article/011/0004451835?cds=news_media_pc"
-url = input('네이버 뉴스 주소: ')
+@tool
+def get_word_length(word: str) -> int:
+    """Returns the length of a word"""
 
-response = requests.get(url)
+    return len(word)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+@tool()
+def multiply_numbers(a: int, b: int) -> int:
+    """Multiply two numbers"""
 
-    title = soup.find('h2', id='title_area').get_text()
-    content = soup.find('div', id='contents').get_text()
+    return a * b
 
-    cleaned_title = re.sub(r'\n{2, }', '\n', title)
-    cleaned_content = re.sub(r'\n{2, }', '\n', content)
+@tool
+def naver_news_crawl(news_url: str) -> str:
+    """Crawls a 네이버(naver.com) news article and returns the body content"""
 
-    print(f'제목: {cleaned_title}')
-    print('본문')
-    print('==' * 20)
-    print(cleaned_content)
+    response = requests.get(news_url)
 
-else:
-    print(f'HTTP 요청 실패 {response.status_code}')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        title = soup.find('h2', id='title_area').get_text()
+        content = soup.find('div', id='contents').get_text()
+
+        cleaned_title = re.sub(r'\n{2, }', '\n', title)
+        cleaned_content = re.sub(r'\n{2, }', '\n', content)
+
+        return f'{cleaned_title}\n{cleaned_content}'
+    else:
+        return f'HTTP 요청 실패 {response.status_code}'
+
+url = "https://n.news.naver.com/article/011/0004451835?cds=news_media_pc"
+answer = naver_news_crawl.invoke({'news_url': url})
+print(answer)
+
