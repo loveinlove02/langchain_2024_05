f01b7c24221bd9cb799eb4903fb21c09215fe085
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-02-19 16:56:13 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,10 +1,40 @@
-prompt = ChatPromptTemplate.from_messages(      # 파이썬 코드 작성을 지시하는 프롬프트
-    [
-        (
-            'system',
-            'You are Raymond Hetting, an expert python programmer, well versed in meta-programming and elegant, concise and short but well documented code. You follow the PEP8 style guide. '
-            'Return only the code, no intro, no explanation, no chatty, no markdown, no code block, no nothing. Just the code.',
-        ),
-        ('human', '{input}'),
-    ]
-)
+from langchain_core.tools import tool
+from typing import List, Dict
+from langchain_teddynote.tools import GoogleNews
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv(verbose=True)
+key = os.getenv('OPENAI_API_KEY')
+
+@tool
+def add_numbers(a: int, b: int) -> int:
+    """Add two numbers"""
+
+    return a + b
+
+@tool()
+def multiply_numbers(a: int, b: int) -> int:
+    """Multiply two numbers"""
+
+    return a * b
+
+@tool
+def search_keyword(query: str) -> List[Dict[str, str]]:
+    """Look up news by keyword"""
+
+    print(f'검색어 : {query}')
+
+    news_tool = GoogleNews()
+
+    return news_tool.search_by_keyword(query, k=3)
+
+
+answer = search_keyword.invoke({'query': '백종원'})
+print(answer)
+
+for i in range(0, len(answer), 1):
+    print(answer[i]['url'])
+    print(answer[i]['content'])
+    print()
