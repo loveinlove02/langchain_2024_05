54f144e7df91830e92b6f55a06e74aa86e2fbb4e
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-04 17:06:54 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,9 +1,14 @@
-from langchain_core.tools import tool
-from typing import List, Dict
-from langchain_teddynote.tools import GoogleNews
+import requests
+import re
+from bs4 import BeautifulSoup
 
+from langchain.agents import tool
 from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers.openai_tools import JsonOutputToolsParser
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+from langchain.agents import create_tool_calling_agent
+from langchain.agents import AgentExecutor
 
 from dotenv import load_dotenv
 import os
@@ -12,69 +17,66 @@ load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
 
 @tool
-def add_numbers(a: int, b: int) -> int:
-    """Add two numbers"""
+def get_word_length(word: str) -> int:
+    """Returns the length of a word"""
+    return len(word)
 
-    return a + b
+@tool()
+def multiply_numbers(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
 
 @tool
-def search_keyword(query: str) -> List[Dict[str, str]]:
-    """Look up news by keyword"""
+def naver_news_crawl(news_url: str) -> str:
+    """Crawls a 네이버(naver.com) news article and returns the body content"""
 
-    print(f'검색어 : {query}')
+    response = requests.get(news_url)
 
-    news_tool = GoogleNews()
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    return news_tool.search_by_keyword(query, k=1)
+        title = soup.find('h2', id='title_area').get_text()
+        content = soup.find('div', id='contents').get_text()
 
+        cleaned_title = re.sub(r'\n{2, }', '\n', title)
+        cleaned_content = re.sub(r'\n{2, }', '\n', content)
 
-# 실제로 도구를 실행 하는 함수
-def execute_tool_calls(tool_call_results):
-    """
-    도구 호출 결과를 실행하는 함수
-
-    :param tool_call_results: 도구 호출 결과 리스트
-    :param tools: 사용 가능한 도구 리스트
-    """
-
-
-    for tool_call_result in tool_call_results:
-        tool_name = tool_call_result['type']
-        tool_agrs = tool_call_result['args']
-
-        matching_tool = None
-
-        for tool in tools:      # [add_numbers, search_keyword]
-            if tool.name == tool_name:  # tool 리스트 이름 == 실행할 이름
-                matching_tool = tool
-                break
-
-        if matching_tool:
-            result = matching_tool.invoke(tool_agrs)
-            print(result)
-        else:
-            print("경고: 실행할 도구를 찾을 수 없습니다.")
+        return f'{cleaned_title}\n{cleaned_content}'
+    else:
+        return f'HTTP 요청 실패 {response.status_code}'
 
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are very powerful assistant, but don't know current events."),
+        ("user", "{input}"),
+        # 에이전트가 검색하는 과정에서 중간 단계를 끄적이는 기능으로..
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ]
+)
 
-# llm (모델 gpt-4o-mini) 만들기
 llm = ChatOpenAI(
     api_key=key,
     model='gpt-4o-mini',
     temperature=0
 )
 
-# 우리가 만들어 놓은 도구(함수) : add_numbers, search_keyword
-tools = [add_numbers, search_keyword]
+# 도구 목록 리스트
+tools = [get_word_length, multiply_numbers, naver_news_crawl]
 
-# llm 에 도구를 쥐여 준다 (llm 에 도구를 바인딩)
-llm_with_tools = llm.bind_tools(tools)
+# 에이전트
+agent = create_tool_calling_agent(llm, tools, prompt)
 
-chain = llm_with_tools | JsonOutputToolsParser(tools=tools)
+# 에이전트 실행기
+agent_executor = AgentExecutor(
+    agent=agent,        # 에이전트
+    tools=tools,        # 내가 만든 도구들 (리스트)
+    verbose=True,       # 중간 단계들의 메시지
+    handle_parsing_errors=True
+)
 
-tool_call_result = chain.invoke('10 더하기 20은?')
-execute_tool_calls(tool_call_result)
-
-# print(tool_call_result)
-# print(tool_call_result[0]['args'])  # 실행할 내용(인자)
-# print(tool_call_result[0]['type'])  # 실행할 도구 이름(함수)
+answer = agent_executor.invoke({"input": "`안녕하세요` 의 길이는?"})
+print(answer)
+print(answer['output'])
