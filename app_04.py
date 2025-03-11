f7a677fe877c1f03705c1c23b93bc3f557ac2e14
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-11 18:23:36 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
@@ -9,6 +11,9 @@ import os
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
 
+# 스트림릿 앱 제목 설정
+st.title("영어 -> 한국어 번역기")
+
 class ConverSationSummary_EN(BaseModel):
     original_english_sentence: str  = Field(description='원문 영어 문장')
     translated_korean_sentence: str = Field(description='번역된 한국어 문장')
@@ -16,7 +21,7 @@ class ConverSationSummary_EN(BaseModel):
 
 parser_en = PydanticOutputParser(pydantic_object=ConverSationSummary_EN)
 
-prompt = load_prompt('./prompts/prompt_eng_to_kor.yaml')
+prompt = load_prompt('./prompts/prompt_eng_to_kor.yaml', encoding='utf-8')
 prompt = prompt.partial(format=parser_en.get_format_instructions())
 
 llm = ChatOpenAI(
@@ -27,9 +32,17 @@ llm = ChatOpenAI(
 
 chain = prompt | llm
 
-user_input = """Let\'s meet at the school gate after class. 
-We can go eat 떡볶이 together
-"""
-response = chain.invoke({'question': user_input})
-print(response.content)
+# 사용자로부터 영어 입력 받기
+user_input = st.text_area("번역할 영어 문장을 입력하세요:")
 
+# 번역 버튼 클릭 시 동작
+if st.button("번역하기"):
+    if user_input:
+        response = chain.invoke({'question': user_input})
+        structed_output = parser_en.parse(response.content)
+
+        st.write(f"원문 문장: {structed_output.original_english_sentence}")
+        st.write(f"번역 문장: {structed_output.translated_korean_sentence}")
+        st.write(f"참고 사항: {structed_output.context_precautions_of_translation}")
+    else:
+        st.write("영어 문장을 입력해주세요.")
