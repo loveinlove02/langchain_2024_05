30f104cd104a18b04a2292c0103b7a701019bc19
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-12 15:57:54 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,4 +1,14 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
 from langchain_teddynote.prompts import load_prompt
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv(verbose=True)
+key = os.getenv('OPENAI_API_KEY')
 
 prompt = load_prompt('./prompts/prompt_kor_to_eng.yaml', encoding='utf-8')
 print(prompt)
