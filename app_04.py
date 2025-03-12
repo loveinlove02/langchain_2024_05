8213abd8d226b85412c2c19b2c0bb7910313c966
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-12 16:16:52 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -15,5 +15,9 @@ class ConverSationSummary_KOR(BaseModel):
     translated_english_sentence: str = Field(description='번역된 영어 문장')
     context_precautions_of_translation: str = Field(description='번역의 맥락이나 주의사항')
 
+parser_kor = PydanticOutputParser(pydantic_object=ConverSationSummary_KOR)
+
 prompt = load_prompt('./prompts/prompt_kor_to_eng.yaml', encoding='utf-8')
+prompt = prompt.partial(format=parser_kor.get_format_instructions())
+
 print(prompt)
