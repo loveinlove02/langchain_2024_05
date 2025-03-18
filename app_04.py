ba0a2cb4684a505c70842f1b56362d09bd464b6f
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-18 17:39:55 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,1 +1,14 @@
-ㅁ
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import MessagesPlaceholder
+
+from langchain.agents import tool
+from langchain.agents import create_tool_calling_agent
+from langchain.agents import AgentExecutor
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv(verbose=True)
+key = os.getenv('OPENAI_API_KEY')
+
