ed6389222e7221ca427c8e9759fa4a08efbb2fd5
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-18 17:53:23 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -4,7 +4,9 @@ from langchain_core.prompts import MessagesPlaceholder
 from langchain.agents import tool
 from langchain.agents import create_tool_calling_agent
 from langchain.agents import AgentExecutor
+
 from langchain_teddynote.tools import GoogleNews
+from typing import List, Dict, Annotated
 
 from dotenv import load_dotenv
 import os
@@ -17,4 +19,16 @@ def add_function(a:float, b:float) -> float:
     """Adds two numbers together."""
     return a+b
 
+@tool
+def search_keyword(query: str) -> List[Dict[str, str]]:
+    """Look up news by keyword"""
 
+    print(f'검색어 : {query}')
+    news_tool = GoogleNews()        # 구글 뉴스 검색 객체
+    answer = news_tool.search_by_keyword(query, k=3)
+
+    return answer
+
+# 테스트
+result = search_keyword.invoke({'query': '봄을 알리는'})
+print(result)
