a6d19a04cacc0297565d0562e4f5def820e79050
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-18 17:46:36 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,14 +1,20 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts import MessagesPlaceholder
-
 from langchain.agents import tool
 from langchain.agents import create_tool_calling_agent
 from langchain.agents import AgentExecutor
+from langchain_teddynote.tools import GoogleNews
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
+
+@tool
+def add_function(a:float, b:float) -> float:
+    """Adds two numbers together."""
+    return a+b
+
 
