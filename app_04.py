9617caf59fc2344dcc1bdf68af8feab05d91cccd
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-19 17:00:10 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,30 +1,21 @@
-aimiessage = AIMessage(
-    content = '실행했습니다.', 
-    add_messages = {'refusal': None},
-    response_metadata = {
-        'token_usage': {
-            'completion_tokens': 243,
-            'prompt_tokens': 21,
-            'total_tokens': 264, 
-            'completion_tokens_details': {
-                'accepted_prediction_tokens': 0,
-                'audio_tokens': 0,
-                'reasoning_tokens': 0, 
-                'rejected_prediction_tokens': 0 
-            },
-            'prompt_tokens_details': {'audio_tokens': 0, 'cached_tokens': 0}
-        }, 
-        'model_name': 'gpt-4o-mini-2024-07-18', 
-        'system_fingerprint': 'fp_b8bc95a0ac', 
-        'finish_reason': 'stop', 
-        'logprobs': None
-    },
-    id='run-058fd8e4-aeb3-4ff1-9951-fde8f3327a2f-1',
-    usage_metadata = {
-    'input_tokens': 21, 
-    'output_tokens': 243, 
-    'total_tokens': 264, 
-    'input_token_details': {'audio': 0, 'cache_read': 0}, 
-    'output_token_details': {'audio': 0, 'reasoning': 0}
-    } 
-)
+from typing import Annotated, TypedDict
+from langgraph.graph import StateGraph, START, END
+from IPython.display import Image, display
+
+class MyState(TypedDict):
+    counter: int
+
+def increment(state: MyState):
+    print('===== increment() 함수 시작 =====')
+    answer = state['counter'] + 1
+    print(f'실행 결과 : {answer}')
+    print('===== increment() 함수  끝 =====')
+
+graph = StateGraph(MyState)
+graph.add_node('increment', increment)  # increment 노드 생성
+
+graph.add_edge(START, 'increment')  # START 에서 increment
+graph.add_edge('increment', END)    # increment 에서 END
+
+# 여기서 다시
+
