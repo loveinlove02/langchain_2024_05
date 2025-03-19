0740c103572983bfab667cfaca86d4bef4d7620a
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-19 16:34:24 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,74 +1,30 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.prompts import MessagesPlaceholder
-from langchain.agents import tool
-from langchain.agents import create_tool_calling_agent
-from langchain.agents import AgentExecutor
-
-from langchain_teddynote.tools import GoogleNews
-from typing import List, Dict, Annotated
-
-from dotenv import load_dotenv
-import os
-
-load_dotenv(verbose=True)
-key = os.getenv('OPENAI_API_KEY')
-
-@tool
-def add_function(a:float, b:float) -> float:
-    """Adds two numbers together."""
-    return a+b
-
-@tool
-def search_keyword(query: str) -> List[Dict[str, str]]:
-    """Look up news by keyword"""
-
-    print(f'검색어 : {query}')
-    news_tool = GoogleNews()        # 구글 뉴스 검색 객체
-    answer = news_tool.search_by_keyword(query, k=3)
-
-    return answer
-
-
-# print(f'도구 이름: {search_keyword.name}')
-# print(f'도구 설명: {search_keyword.description}')
-
-# 2개의 도구를 리스트에 넣는다
-tools = [search_keyword, add_function]
-
-# Agent prompt
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. "             
-            "Make sure to use the `search_news` tool for searching keyword related news."
-        ),
-        ("placeholder", "{chat_history}"),          # 이전 대화 내용을 넣을 곳을 잡아둔다.
-        ("human", "{input}"),                       # 사용자 입력
-        ("placeholder", "{agent_scratchpad}"),
-    ]
+aimiessage = AIMessage(
+    content = '실행했습니다.', 
+    add_messages = {'refusal': None},
+    response_metadata = {
+        'token_usage': {
+            'completion_tokens': 243,
+            'prompt_tokens': 21,
+            'total_tokens': 264, 
+            'completion_tokens_details': {
+                'accepted_prediction_tokens': 0,
+                'audio_tokens': 0,
+                'reasoning_tokens': 0, 
+                'rejected_prediction_tokens': 0 
+            },
+            'prompt_tokens_details': {'audio_tokens': 0, 'cached_tokens': 0}
+        }, 
+        'model_name': 'gpt-4o-mini-2024-07-18', 
+        'system_fingerprint': 'fp_b8bc95a0ac', 
+        'finish_reason': 'stop', 
+        'logprobs': None
+    },
+    id='run-058fd8e4-aeb3-4ff1-9951-fde8f3327a2f-1',
+    usage_metadata = {
+    'input_tokens': 21, 
+    'output_tokens': 243, 
+    'total_tokens': 264, 
+    'input_token_details': {'audio': 0, 'cache_read': 0}, 
+    'output_token_details': {'audio': 0, 'reasoning': 0}
+    } 
 )
-
-# llm 만들기
-llm = ChatOpenAI(
-    api_key=key,
-    model='gpt-4o-mini',
-    temperature=0
-)
-
-# 에이전트 생성 : llm + 도구들 + 프롬프트
-agent = create_tool_calling_agent(llm, tools, prompt)
-
-# 에이전트 실행기 생성
-agent_executor = AgentExecutor(
-    agent=agent,        # 각 단계에서 계획을 생성하고 행동을 결정하는 에이전트
-    tools=tools,        # 에이전트가 사용할 도구 리스트(함수)
-    verbose=False,      # 중단 단계 출력
-    max_iterations=10,  # 최대 반복 횟수
-    max_execution_time=10,  # 실행되는데 소요되는 최대 시간
-    handle_parsing_errors=True
-)
-
-answer = agent_executor.invoke({'input': '10+20의 결과는?'})
-print(answer)
