1795f483178b20f69969b61b043ebf77fe8d81fd
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-03-26 15:44:37 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,1 +1,18 @@
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv(verbose=True)
+key = os.getenv('OPENAI_API_KEY')
+
+llm = ChatOpenAI(
+    api_key=key,
+    model='gpt-4o-mini',
+    temperature=0
+)
+
+qusetion = HumanMessage(content='대한민국의 수도는 어디야?', id='1')
+answer = llm.invoke(qusetion.content)
+print(answer)
