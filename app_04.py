a3f63fb160053750598360a8eb06ab5569dc1773
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-04-08 18:21:57 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -22,39 +22,53 @@ def search_keyword(query: str) -> List[Dict[str, str]]:
     news_tool = GoogleNews()
     return news_tool.search_by_keyword(query, k=2)
 
-anwer = search_keyword.invoke({'query': '6월 선거'})
-print(anwer)
-
-
 @tool
-def python_repl_tool(
-    code: Annotated[str, "The python code to execute to generate your chart."],
-):
+def python_repl_tool(code: Annotated[str, "The python code to execute to generate your chart."]):
     """Use this to execute python code. If you want to see the output of a value,
-    you should print it out with `print(...)`. This is visible to the user."""
+       you should print it out with `print(...)`. This is visible to the user."""
 
     result = ""
-    
+
     try:
         result = PythonREPL().run(code)
     except BaseException as e:
-        print(f"Failed to execute. Error: {repr(e)}")
+        print(f"오류: {repr(e)}")
     finally:
         return result
 
+tools = [search_keyword, python_repl_tool]
 
-# 프롬프트는 에이전트에게 모델이 수행할 작업을 설명하는 텍스트를 제공합니다. (도구의 이름과 역할을 입력)
+# 1. 에이전트 프롬프트
 prompt = ChatPromptTemplate.from_messages(
     [
-        (                                           
-            "system",
-            "You are a helpful assistant. "             
-            "Make sure to use the `search_news` tool for searching keyword related news."
+        (
+            'system',
+            'You are a helpful assistant. '
+            'Make sure to use the `search_keyword` tool for searching keyword related news'
         ),
-        ("placeholder", "{chat_history}"),          # 이전 대화 내용을 넣을 곳을 잡아둔다.
-        ("human", "{input}"),                       # 사용자 입력
-
-        # 에이전트가 검색하는 과정들이나 내용 등을 끄적이는 메모장 같은 공간을 플레이스 홀더로 만들어준다
-        ("placeholder", "{agent_scratchpad}"),       
+        ('placeholder', '{chat_history}'),
+        ('human', '{input}'),
+        ('placeholder', '{agent_scratchpad}')
     ]
 )
+
+# 2. LLM
+llm = ChatOpenAI(
+    api_key=key,
+    model='gpt-4o-mini',
+    temperature=0
+)
+
+# 3.에이전트 생성
+agent = create_tool_calling_agent(llm, tools, prompt)
+
+# 4.에이전트 실행기(AgentExecutor)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True,
+    handle_parsing_errors=True
+)
+
+answer = agent_executor.invoke({'input': '선거'})
+print(answer)
