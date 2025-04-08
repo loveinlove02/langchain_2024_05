ff0c3ae52e66fac16d9ac28987798d333e13d4dd
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-04-08 18:01:13 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -24,3 +24,20 @@ def search_keyword(query: str) -> List[Dict[str, str]]:
 
 anwer = search_keyword.invoke({'query': '6월 선거'})
 print(anwer)
+
+
+@tool
+def python_repl_tool(
+    code: Annotated[str, "The python code to execute to generate your chart."],
+):
+    """Use this to execute python code. If you want to see the output of a value,
+    you should print it out with `print(...)`. This is visible to the user."""
+
+    result = ""
+    
+    try:
+        result = PythonREPL().run(code)
+    except BaseException as e:
+        print(f"Failed to execute. Error: {repr(e)}")
+    finally:
+        return result
