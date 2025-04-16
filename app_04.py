094e53710df2f74ad77defac1a733c9bb5fb1f10
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-04-16 16:41:54 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,9 +1,13 @@
 from langchain.agents import tool
 from typing import List, Dict, Annotated
+
 from langchain_teddynote.tools import GoogleNews
 from langchain_experimental.utilities import PythonREPL
+
+
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+
 from langchain.agents import create_tool_calling_agent
 from langchain.agents import AgentExecutor
 
@@ -12,3 +16,29 @@ import os
 
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
+
+@tool
+def search_keyword(query: str) -> List[Dict[str, str]]:
+    """Look up news by keyword"""
+
+    print(f'검색어: {query}')
+    news_tool = GoogleNews()
+
+    return news_tool.search_by_keyword(query, k=1)
+
+
+@tool
+def python_repl_tool(
+        code: Annotated[str, "The python code to execute to generate your chart."],
+):
+    """Use this to execute python code. If you want to see the output of a value,
+    you should print it out with `print(...)`. This is visible to the user."""
+
+    result = ""
+
+    try:
+        result = PythonREPL().run(code)
+    except BaseException as e:
+        print(f"Failed to execute. Error: {repr(e)}")
+    finally:
+        return result
