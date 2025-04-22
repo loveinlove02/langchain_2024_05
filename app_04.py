f07379053623aa726cf27ae28544689a3bd5dad1
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-04-22 18:37:39 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,50 +1,23 @@
-from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain.document_loaders import PyMuPDFLoader
-
-from langchain.tools.retriever import create_retriever_tool
 from langchain_community.tools.tavily_search import TavilySearchResults
-
 from dotenv import load_dotenv
 import os
 
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
+tavily_key = os.getenv('TAVILY_API_KEY')
 
-# 웹 검색
-search = TavilySearchResults(k=5)
-
-# 문서 검색
-
-# 1.문서 로드
-loader = PyMuPDFLoader('./data/SPRI_AI_Brief_2023년12월호_F.pdf')
-docs = loader.load()
-
-# 2.문서 분할
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-split_document = text_splitter.split_documents(docs)
-
-# 3.임베딩 생성
-embeddings = OpenAIEmbeddings(
-    api_key=key,
-    model='text-embedding-3-small'
+tool = TavilySearchResults(
+    key=tavily_key,
+    max_results=5,                      # 반환되는 최대 검색 결과 수(기본값: 5)
+    include_answer=False,               # 원본 질문(쿼리)에 대한 짧은 답변 포함 여부
+    include_raw_content=False,          # HTML 컨텐츠 여부
+    include_domains=['www.naver.com', 'namu.wiki'],
+    # exclude_domains=[]
 )
+answer = tool.invoke({'query': '농심 안성탕면'})
+print(answer)
 
-# 4. DB 생성 및 저장
-vectorstore = FAISS.from_documents(documents=split_document, embedding=embeddings)
-
-# 5.벡터스토어에 들어가서 검색해오는 검색기(Retriever) 생성
-retriever = vectorstore.as_retriever()
-
-retriever_tool = create_retriever_tool(
-    retriever,               # 리트리버
-    name='pdf_search',       # 도구의 이름
-    description='여기서 도구에 대한 설명....'
-)
-
-# answer = retriever.invoke('삼성전자가 개발한 생성형 AI이름?')
-# # print(answer)
-#
-# for data in answer:
-#     print(data)
+for data in answer:
+    print(data['title'])
+    print(data['url'])
+    print(data['content'])
