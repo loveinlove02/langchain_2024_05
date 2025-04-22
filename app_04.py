a9c41288bd0ca405a925e9727c5194813dedecb9
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-04-22 18:06:33 +0900

    Update app_04.py

--- app_04.py
+++ app_04.py
@@ -1,23 +1,23 @@
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.document_loaders import PyMuPDFLoader
+
 from langchain_community.tools.tavily_search import TavilySearchResults
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
-tavily_key = os.getenv('TAVILY_API_KEY')
 
-tool = TavilySearchResults(
-    key=tavily_key,
-    max_results=5,                      # 반환되는 최대 검색 결과 수(기본값: 5)
-    include_answer=False,               # 원본 질문(쿼리)에 대한 짧은 답변 포함 여부
-    include_raw_content=False,          # HTML 컨텐츠 여부
-    include_domains=['www.naver.com', 'namu.wiki'],
-    # exclude_domains=[]
-)
-answer = tool.invoke({'query': '농심 안성탕면'})
-print(answer)
+# 웹 검색
+search = TavilySearchResults(k=5)
 
-for data in answer:
-    print(data['title'])
-    print(data['url'])
-    print(data['content'])
+# 문서 검색
+
+# 1.문서 로드
+loader = PyMuPDFLoader('./data/SPRI_AI_Brief_2023년12월호_F.pdf')
+docs = loader.load()
+
+# 2.
