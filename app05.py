b1a3d0978f4667fab67565286467b243d69d3bfa
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-05-07 15:48:23 +0900

    Update app05.py

--- app05.py
+++ app05.py
@@ -15,3 +15,47 @@ import os
 
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
+
+# ====================
+# 문서 검색기
+# ====================
+
+# 단계 1: 문서 로드(Load Documents)
+loader = PyMuPDFLoader('data/SPRI_AI_Brief_2023년12월호_F.pdf')
+docs = loader.load()
+
+# 단계 2: 문서 분할(Split Documents)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+split_documents = text_splitter.split_documents(docs)
+
+# 단계 3: 임베딩(Embedding) 생성
+embeddings = OpenAIEmbeddings(
+    api_key=key,
+    model='text-embedding-3-small'
+)
+
+# 단계 4: DB 생성(Create DB) 및 저장
+# 벡터스토어 생성합니다.
+vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
+
+# 단계 5: 검색기(Retriever) 생성
+retriever = vectorstore.as_retriever()
+
+
+# ====================
+# 도구 만들기
+# ====================
+
+# 1. 문서검색 도구
+retriever_tool = create_retriever_tool(
+    retriever=retriever,        # 검색기(retriever)
+    name='pdf_search',          # 도구의 이름
+    description='use this tool to search information from the PDF document'
+)
+
+# 2. 웹 검색 도구
+search = TavilySearchResults(k=2) 
+
+# 3.도구 목록
+tools = [retriever_tool, search]
+
