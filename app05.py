28428531e56dc3b5e345c045c2c3b4d7356bf80b
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-05-09 20:38:48 +0900

    Update app05.py

--- app05.py
+++ app05.py
@@ -26,3 +26,37 @@ def lastest_news(k: int = 5) -> List[Dict[str, str]]:
     news_tool = GoogleNews()
     
     return news_tool.search_latest(k=k)
+
+
+# 폴더 생성하고 시작.
+if not os.path.exists('tmp'):   # tmp 라는 폴더가 없으면
+    os.mkdir('tmp')             # tmp 라는 이름의 폴더를 만든다
+
+
+working_directory = 'tmp'
+
+tools = FileManagementToolkit(
+    root_dir=str(working_directory)
+).get_tools()
+
+# 파일 도구, 뉴스 검색 도구
+tools.append(lastest_news)
+
+llm = ChatOpenAI(
+    api_key=key, 
+    model='gpt-4o-mini'
+)
+
+prompt = ChatMessagePromptTemplate.format_messages(
+    [
+        (
+            'system',
+            'You are a helpful assistant. '
+            'Make sure to use `lastest_news` tool to find lastest news. '
+            'Make sure to use `file_management` tool to manage files. '
+        ),
+        ('placeholder', '{chat_history}'),
+        ('human', '{input}'),
+        ('placeholder', '{agent_scratchpad}')
+    ]
+)
