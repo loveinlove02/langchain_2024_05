6dc6e7c8278403f85125384dda8dd3a0579619a0
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-05-15 19:29:08 +0900

    Update app_03.py

--- app_03.py
+++ app_03.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 from dotenv import load_dotenv
 import os
@@ -15,5 +17,9 @@ docs = loder.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
 split_documents = text_splitter.split_documents(docs)
 
-print(split_documents)
-print(len(split_documents))
+# 3. 임베딩
+embeddings = OpenAIEmbeddings(api_key=key, model='text-embedding-3-small')
+
+# 4. 데이터베이스 생성 하고 임베딩을 사용해서 잘라놓은 문서를 저장
+vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
+print(vectorstore)
