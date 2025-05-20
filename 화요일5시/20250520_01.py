6ae38b947f15ecfe52bfa3a41b51e1e884ae5640
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-05-20 17:19:37 +0900

    Create 20250520_01.py

---
+++ 화요일5시/20250520_01.py
@@ -0,0 +1,13 @@
+"""
+FileManagementToolkit
+
+사용자 컴퓨터의 파일 관리를 위한 도구 모음.
+
+- CopyFileTool   : 파일 복사
+- DeleteFileTool : 파일 삭제
+- FileSearchTool : 파일 검색
+- MoveFileTool : 파일 이동
+- ReadFileTool : 파일 읽기
+- WriteFileTool  : 파일 쓰기
+- ListDirectoryTool : 디렉토리 목록 조회
+"""
