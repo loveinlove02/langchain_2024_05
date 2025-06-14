9d254d820c5f0d995454e2b9dbb8872117d0ece1
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-06-14 15:05:15 +0900

    Update 토요일2시.py

--- 토요일2시.py
+++ 토요일2시.py
@@ -48,6 +48,8 @@ llm_with_tools = llm.bind_tools(tools)
 
 # chain
 chain = llm_with_tools | JsonOutputToolsParser(tools=tools)
+
+"""
 tool_call_result = chain.invoke('이스라엘 공격')
 
 print(tool_call_result)
@@ -61,3 +63,40 @@ print(tool_call_result[0]['args'])
 
 print(tool_call_result[0]['type'])
 # search_keyword
+"""
+
+def execute_tool_calls(tool_call_results):
+    """
+    도구 호출 결과를 실행하는 함수
+    :param tool_call_result: 도구 호출 결과 리스트
+    [{'args': {'query': '이스라엘 공격'}, 'type': 'search_keyword'}]
+    :return: 사용 가능한 도구 리스트
+    """
+    print(f'tool_call_result: {tool_call_results}')
+    print()
+
+    for tool_call_result in tool_call_results:
+        tool_name = tool_call_result['type']        # 도구 이름 : search_keyword
+        tool_agrs = tool_call_result['args']        # 인자 : {'query': '이스라엘 공격'}
+
+        matching_tool = None
+
+        for tool in tools:  # [add_numbers, multiply_numbers, search_keyword]
+            if tool.name == tool_name:
+                matching_tool = tool
+                break
+
+        if matching_tool:
+            result = matching_tool.invoke(tool_agrs)
+
+            print(f'[실행된 도구]: {matching_tool.name}')
+            print(f'[실행 결과]  : {result}')
+        else:
+            print(f'{tool_name} 도구를 찾을 수 없습니다.')
+
+
+tool_call_result = chain.invoke('이스라엘 공격')
+execute_tool_calls(tool_call_result)
+
+
+
