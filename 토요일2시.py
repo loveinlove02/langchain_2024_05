f0c14a192b22612dddeaec2578b1268005bc3336
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-06-14 14:31:46 +0900

    Update 토요일2시.py

--- 토요일2시.py
+++ 토요일2시.py
@@ -1,9 +1,6 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableLambda
-
-from langchain_experimental.tools import PythonAstREPLTool
+from langchain.tools import tool
+from typing import List, Dict
+from langchain_teddynote.tools import GoogleNews
 
 from dotenv import load_dotenv
 import os
@@ -11,29 +8,27 @@ import os
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
 
-def print_and_execute(code):
-    python_tool = PythonAstREPLTool()
-    print('코드:')
-    print(code)
+@tool
+def add_numbers(a: int, b: int) -> int:
+    """Add two numbers"""
 
-    return python_tool.invoke(code)
+    return a+b
 
-# 1.프롬프트
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            'system',
-            '당신은 Raymond Hetting으로, 메타프로그래밍에 능숙하고 우아하며 간결하고 짧지만 문서화가 잘 된 코드를 작성하는 파이썬 전문가 입니다.'
-            'PEP8 스타일 가이드를 준수합니다. 소개, 설명, 잡담, 마크다운, 코드 불록 등은 모두 없이 코드만 출력하세요.'
-        ),
-        ('human', '{input}')
-    ]
-)
+@tool
+def multiply_numbers(a: int, b: int) -> int:
+    """Multiply two numbers"""
 
-'''
-(
-    'system',
-    'You are Raymond Hetting, an expert python programmer, well versed in meta-programming and elegant, concise and short but well documented code. You follow the PEP8 style guide. '
-    'Return only the code, no intro, no explanation, no chatty, no markdown, no code block, no nothing. Just the code.',
-)
-'''
+    return a*b
+
+@tool
+def search_keyword(query: str) -> List[Dict[str, str]]:
+    """Look up new by keyword"""
+
+    print(f'검색어: {query}')
+    news_tool = GoogleNews()
+
+    return news_tool.search_by_keyword(keyword=query, k=2)
+
+
+res = search_keyword.invoke({'query': '김민석 의원'})
+print(res)
