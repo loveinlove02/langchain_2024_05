9fd105df6e4bac2191a955dd8b8094e0f0fc5dfd
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-07-16 15:59:46 +0900

    Update fastapi02.py

--- 수요일3시30/fastapi02.py
+++ 수요일3시30/fastapi02.py
@@ -7,6 +7,10 @@ app = FastAPI()
 async def read_all_items():
     return {"message" : "all_items"}
 
+# http://127.0.0.1:8002/items/2
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id" : item_id}
 
 
 # uvicorn 파일이름:변수이름 --port=8002 --reload
