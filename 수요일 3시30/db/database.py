5ddc1c001d40c93e25d6bab921e49d4d49e42e3c
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-08-29 20:16:40 +0900

    Update database.py

--- 수요일 3시30/db/database.py
+++ 수요일 3시30/db/database.py
@@ -1,1 +1,25 @@
-a
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.pool import QueuePool
+from fastapi import status
+from fastapi.exceptions import HTTPException
+
+
+DATABASE_CONN = "mysql+mysqlconnector://root:1234@127.0.0.1:3306/blog_db2"
+
+engine = create_engine(DATABASE_CONN, poolclass=QueuePool, 
+                       pool_size=10, max_overflow=0)
+
+def context_get_conn():
+    conn = None
+
+    try:
+        conn = engine.connect()
+        yield conn
+    except SQLAlchemyError as e:
+        print(e)
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE ,
+                            detail="오류가 발생했습니다.")
+    finally:
+        if conn:
+            conn.close()
