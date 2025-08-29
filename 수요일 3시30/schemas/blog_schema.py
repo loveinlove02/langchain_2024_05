1cc16437b630bf669520b5e9d8114a00fbbf21d3
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-08-29 20:21:46 +0900

    Update blog_schema.py

--- 수요일 3시30/schemas/blog_schema.py
+++ 수요일 3시30/schemas/blog_schema.py
@@ -1,1 +1,12 @@
-a
+from pydantic.dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+@dataclass
+class BlogData:
+    id: int
+    title: str
+    author: str
+    content: str
+    modified_dt: datetime
+    image_loc: str | None = None
