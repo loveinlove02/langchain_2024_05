d49be4a4b38696733db9125cb6bfe3012616d1c1
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-08-29 20:50:01 +0900

    Update blog.py

--- 수요일 3시30/routes/blog.py
+++ 수요일 3시30/routes/blog.py
@@ -1,1 +1,14 @@
-a
+from fastapi import APIRouter, Request, Depends, status
+from fastapi.responses import RedirectResponse
+from fastapi.exceptions import HTTPException
+from fastapi.templating import Jinja2Templates
+from db.database import context_get_conn
+from sqlalchemy.exc import SQLAlchemyError
+
+from schemas.blog_schema import BlogData
+
+router = APIRouter(prefix="/blogs", tags=["blogs"])
+
+templates = Jinja2Templates(directory="templates") 
+
+# http://127.0.0.1:8002/blogs
